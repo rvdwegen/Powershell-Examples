e14e5186b27adad3dedc12171916f5dfe504905d
Roel van der Wegen <github@aeternus.tech>
2022-09-21 21:37:22 +0200

    Update ReturnPSobjectInFunction.ps1

--- ReturnPSobjectInFunction.ps1
+++ ReturnPSobjectInFunction.ps1
@@ -1,10 +1,12 @@
 Function Get-PSObjectExample {
 		$value1 = "value1"
 		$value2 = "value2"
+		$value3 = "value3"
 
     $obj = New-Object -TypeName PSObject
     $obj | Add-Member -MemberType NoteProperty -Name 'value1' -Value $value1
-    $obj | Add-Member -MemberType NoteProperty -Name 'value1' -Value $value2
+    $obj | Add-Member -MemberType NoteProperty -Name 'value2' -Value $value2
+    $obj | Add-Member -MemberType NoteProperty -Name 'value3' -Value $value3
 
 
     # Return the object
